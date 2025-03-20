3e08bf9f0022aedaed5cdc85f478b366806e1a14
Marcos Willian <marcoswms@gmail.com>
2025-03-20 20:49:51 -0300

    feat: adicionado entidades ao diretório: model

---
+++ src/model/Cliente.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Calendar;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+    private Calendar data_nascimento;
+    private String endereco;
+
+    public Cliente() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/model/Contato.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Contato {
+    private int id;
+    private int cliente_id;
+    private String tipo;
+    private String valor;
+    private String observacao;
+
+    public Contato() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}
