88a8750d4a226e85d0f413397db48b249a8aa6fe
Marcos Willian <marcoswms@gmail.com>
2025-03-21 02:45:02 -0300

    feat: adicionado configuração de conexão do banco de dados

---
+++ src/config/ConexaoBancoDeDados.java
@@ -0,0 +1,28 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConexaoBancoDeDados {
+    private static final String url = "jdbc:mysql://localhost:3306/projeto_agenda";
+    private static final String usuario = "root";
+    private static final String senha = "root";
+
+    private static Connection conexao;
+
+    public static Connection getConexao() {
+        try {
+            if (conexao == null) {
+                conexao = DriverManager.getConnection(url, usuario, senha);
+                return conexao;
+            } else {
+                return conexao;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
